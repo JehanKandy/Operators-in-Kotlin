162bdc00e5415ef7c1f5a6ed0ec1c7eac84bb013
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-12-17 23:14:24 +0530

    Add files via upload

---
+++ operators.kt
@@ -0,0 +1,30 @@
+fun main(){
+    //arithmetic Operators
+    //+,-,*,/,%
+
+    val num1 = 12.0
+    val num2 = 10.0
+    var result:Double
+
+    print("Num1 = $num1 , Num2 = $num2 \n\n")
+    //Addition 
+    result = num1 + num2
+    print("Num1 + Num2 =  $result \n")
+
+    //Substraction
+    result = num1 - num2
+    print("Num1 - Num2 = $result \n")
+
+    //Multiplication 
+    result = num1 * num2
+    print("Num1 * Num2 = $result \n")
+
+    //Division
+    result = num1 / num2
+    print("Num1 / Num2 = $result \n")
+
+    //Modification 
+    result = num1 % num2
+    print("Num1 % Num2 = $result \n")
+
+}
